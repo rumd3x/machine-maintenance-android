7a6a775cfb13aec00262607a564cd55627f55711
Jenkins CI <jenkins@ci.local>
2026-02-10 02:20:50 +0000

    chore: bump version to 0.1.0+6

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 // App version - single source of truth for display purposes
 // Note: pubspec.yaml version is used for build/packaging
-const String appVersion = '0.0.2';
-const int appBuildNumber = 5;
+const String appVersion = '0.1.0';
+const int appBuildNumber = 6;
 
 // Maintenance types
 const String maintenanceTypeOilChange = 'oil_change';
